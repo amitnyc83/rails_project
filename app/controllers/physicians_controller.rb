f16473af9009056c02289070bb7698f0355fe73c
amitnyc83 <amit_nyc@live.com>
2019-01-08 12:53:41 -0500

    performs CRUD for physician

--- app/controllers/physicians_controller.rb
+++ app/controllers/physicians_controller.rb
@@ -1,5 +1,5 @@
 class PhysiciansController < ApplicationController
-
+  before_action :find_physician, [:index, :new, :create]
 
   def index
     @physicians = Physician.all
@@ -7,7 +7,7 @@ class PhysiciansController < ApplicationController
 
 
   def show
-    @physician = Physician.find_by(id: params[:id])
+    @appointments = Appointment.where("physician_id = ?", @physician.id)
   end
 
 
@@ -17,7 +17,41 @@ class PhysiciansController < ApplicationController
 
 
   def create
-  end
+      @physician = Physician.new(physician_params)
+      if @physician.save
+        flash[:notice] = "You have successfully created an account"
+        redirect_to physician_path(@physician)
+      else
+        flash[:notice] = "There was an error creating a new Physician"
+        render 'physician/new'
+      end
+    end
+
+    def edit
+    end
+
+
+    def update
+      if @physician.update(physician_params)
+        redirect_to physician_path(@physician)
+      else
+        flash[:notice] = "Error updating Info! Plz make sure all fileds are filled out."
+        render 'physician/new'
+      end
+    end
+
+
+
+    private
+
+    def find_physician
+      @physician = Physician.find_by(params[:id])
+    end
+
+
+    def physician_params
+      params.require(:physician).permit(:name, :specialty, :email, :password)
+    end
 
 
 end
