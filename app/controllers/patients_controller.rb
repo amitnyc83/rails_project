ec0ce13cb973b842ec5b7b1532ec234782a5d934
amitnyc83 <amit_nyc@live.com>
2019-01-09 08:40:45 -0500

    create a patient sign up form

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,51 @@
 class PatientsController < ApplicationController
-  def new
+ before_action :find_patient, only: [:show, :edit, :update]
+
+
+  def index
+    @patients = Patient.all
   end
+
+
+  def new
+    @patient = Patient.new
+  end
+
+  def show
+    @appointments = Appointment.where("patient_id = ?", @patient.id)
+  end
+
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      flash[:notice] = "You have successfully created an account"
+      redirect_to patient_path(@patient)
+    else
+      flash[:notice] = "Ther was an error "
+  end
+end 
+
+
+  def edit
+  end
+
+
+  def update
+  end
+
+
+  private
+
+  def find_patient
+    @patient = Patient.find(params[:id])
+  end
+
+
+  def patient_params
+    params.require(:patient).permit(:name, :age, :password_digest, :email, :date_of_birth)
+  end
+
+
+
 end
