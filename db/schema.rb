ad8d8cc4ebc22c1574f00a996c4941ae91a1cd2b
amitnyc83 <amit_nyc@live.com>
2019-01-16 11:25:13 -0500

    adds slug to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_161140) do
+ActiveRecord::Schema.define(version: 2019_01_16_162355) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "physician_id"
