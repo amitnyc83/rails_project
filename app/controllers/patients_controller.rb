d7f66a4e8bacd11028c6eecc92e0aba8dec86626
amitnyc83 <amit_nyc@live.com>
2019-01-17 10:59:21 -0500

    refactoring

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-before_action :login_required, except: [:new, :create]
+before_action :login_required, except: [:index, :show, :new, :create]
 before_action :find_patient, except: [:new, :create]
 
 
