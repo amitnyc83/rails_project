513e9eacee0b6a1d0e948b69ac0dab11177f0a5e
amitnyc83 <amit_nyc@live.com>
2019-03-07 22:58:33 -0500

    creates a class prototype method and returns it to the DOm with getPhysician function

--- app/assets/javascripts/physicians.js
+++ app/assets/javascripts/physicians.js
@@ -33,3 +33,29 @@ function getPhysicians(){
       })
   })
 }
+
+// JavaScript Object Model
+class Physician{
+  constructor(obj){
+    this.id = obj.id,
+    this.name = obj.name,
+    this.email = obj.email,
+    this.specialty = obj.specialty,
+    this.slug = obj.slug,
+    this.appointments =obj.appointments;
+  }
+}
+
+
+
+// After creating a instance of Object using JavaScript Object Model class, i can create a custom method on that instance using prototype
+Physician.prototype.numOfAppointmentsHTML = function (){
+  return (`
+    <div>
+     <fieldset>
+       <h3>${this.name} has created ${this.appointments.length} appointments.</h3>
+       <p>Email ${this.name} at ${this.email} if you have any questions or would like to schedule an appointment.</p>
+      </fieldset>
+    </div>
+  `)
+}
